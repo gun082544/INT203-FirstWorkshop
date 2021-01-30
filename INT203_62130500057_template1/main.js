d9878ce7aef3721a4640d74b132c2083eb93ce31
gun082544 <gun082544@hotmail.com>
2021-01-31 00:11:06 +0700

    [UPDATE] managed files

---
+++ INT203_62130500057_template1/main.js
@@ -0,0 +1,11 @@
+const app ={
+    data(){
+        return{
+            titleApp: 'Instagram',
+            nameApp: 'Instagram',
+            storyFunctionmsg: 'Story'
+        }
+    }
+}
+
+Vue.createApp(app).mount('#app')
--- main.js
+++
@@ -1,9 +0,0 @@
-const app ={
-    data(){
-        return{
-            msg: 'Hello NongGun'
-        }
-    }
-}
-
-Vue.createApp(app).mount('#app')
